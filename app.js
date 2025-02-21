c30593d7a83d1d9c71636396d6ffee8d4fa31ae6
carlos_al <126424515+carlosAlHer@users.noreply.github.com>
2025-02-21 03:40:49 -0500

    aplicando cors

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const  db_con = require('./db/conexion.js');
 const app = express();
 const port = 3000;
@@ -6,6 +7,7 @@ const port = 3000;
 // Dar formato a los datos que lleguen en el body(del req)
 app.use(express.json());//convierte en json los datos que llegan con un content type json 
 app.use(express.urlencoded({extended: true}))//convierte en json los datos que lleguen en formato form-urlencode
+app.use(cors());
 
 const categRoutes = require('./routing/categories.js');
 const prdRoutes = require('./routing/products.js');
